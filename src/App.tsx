44c3f0386307a9132136d495e5944f37207e3f5f
Gustavo Pineda <gustavopineda95@gmail.com>
2023-03-11 11:28:36 -0600

    Primer Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+
+import { IonApp, setupIonicReact, IonContent, IonHeader, IonPage, IonTitle, IonFooter, IonToolbar} from '@ionic/react';
+
+import './App.css';
+import Pantalla from './components/Pantalla';
+import Boton from './components/Boton';
+import BotonClear from './components/BotonClear';
+import {useState} from 'react';
+import {evaluate} from 'mathjs';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -24,19 +29,70 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [input, setInput] = useState('22318.60');
+  const agregarInput = (val:string) => {
+    setInput(input + val);
+  }
+  const calcularResultado = () => {
+    if(input)
+    setInput(evaluate(input));
+  }
 
+  return (
+    <IonApp>
+      <IonPage>
+        <IonContent fullscreen>
+          <div className="App">
+            <div className="calculadora">
+              <Pantalla input={input}/>
+              <div className='fila'>
+              <Boton manejarClic={agregarInput}>%</Boton>
+              <Boton manejarClic={agregarInput}>CE</Boton>
+              <Boton manejarClic={agregarInput}>C</Boton>
+              <Boton manejarClic={agregarInput}>x</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={agregarInput}>1/x</Boton>
+              <Boton manejarClic={agregarInput}>x2</Boton>
+              <Boton manejarClic={agregarInput}>2/-x</Boton>
+              <Boton manejarClic={agregarInput}>/</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={agregarInput}>7</Boton>
+              <Boton manejarClic={agregarInput}>8</Boton>
+              <Boton manejarClic={agregarInput}>9</Boton>
+              <Boton manejarClic={agregarInput}>*</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={calcularResultado}>=</Boton>
+              <Boton manejarClic={agregarInput}>0</Boton>
+              <Boton manejarClic={agregarInput}>.</Boton>
+              <Boton manejarClic={agregarInput}>/</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={calcularResultado}>=</Boton>
+              <Boton manejarClic={agregarInput}>0</Boton>
+              <Boton manejarClic={agregarInput}>.</Boton>
+              <Boton manejarClic={agregarInput}>/</Boton>
+            </div>
+            <div className='fila'>
+              <Boton manejarClic={calcularResultado}>=</Boton>
+              <Boton manejarClic={agregarInput}>0</Boton>
+              <Boton manejarClic={agregarInput}>.</Boton>
+              <Boton manejarClic={agregarInput}>/</Boton>
+            </div>
+            <div className='fila'>
+              <BotonClear manejarClear={() => setInput('')}>
+                Limpiar
+              </BotonClear>
+            </div>
+          </div>
+        </div>
+      </IonContent>      
+    </IonPage>  
+  </IonApp>
+  )
+  };
 export default App;
+
